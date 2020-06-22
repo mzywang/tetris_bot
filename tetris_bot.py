08f51c28bacce68a628d0b36d6dae34eb630f60a
Michael Wang <mwang@Michaels-MacBook-Pro.local>
2020-06-22 01:19:28 -0400

    typo in comment string

--- tetris_bot.py
+++ tetris_bot.py
@@ -14,7 +14,7 @@ def decide_and_place(block, board):
     Blocks have the following attributes:
      - color: color of the block (blue, green, etc.)
      - block_type: type of block (J, Z, etc.) which corresponds to the color
-     - array: a mxn array where 1's space the block occupies and 0's represent empty space
+     - array: a mxn array where 1's represent space the block occupies and 0's represent empty space
      - rotation_angle: 0, 90, 180, 270 depending on the rotation of the block
      - offset: number of empty blocks from the LHS of the board to the RHS of the block
     """
